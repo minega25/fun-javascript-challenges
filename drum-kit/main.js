f25254f7841f3d50ef2972dd10bdadff6a2c325a
minega25 <minega25@gmail.com>
2024-01-04 23:54:21 +0200

    Add on click event handler

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -1,21 +1,33 @@
-window.addEventListener("keydown", ({ keyCode }) => {
+window.addEventListener('keydown', ({ keyCode }) => {
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${keyCode}"]`);
   if (!audio) return;
 
-  key.classList.add("playing");
+  key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
 });
 
-const keys = document.querySelectorAll(".key");
+window.addEventListener('click', (e) => {
+  const domElement = e.target;
+  const dataKey = domElement.getAttribute('data-key');
+  const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+
+  const key = document.querySelector(`.key[data-key="${dataKey}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+});
+
+const keys = document.querySelectorAll('.key');
 
 keys.forEach((key) => {
-  key.addEventListener("transitionend", removePlaying);
+  key.addEventListener('transitionend', removePlaying);
 });
 
 function removePlaying(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove("playing");
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
 }
-
